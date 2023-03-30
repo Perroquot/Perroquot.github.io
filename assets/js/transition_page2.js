e831a341188c6ac27a156cee67e11da69a750335
Perroquot <trystan.colin80@gmail.com>
2023-03-30 15:36:41 +0200

    dqzdq

---
+++ assets/js/transition_page2.js
@@ -0,0 +1,42 @@
+content_page1 = [
+    `<h3><strong>Partie 1 : Marco Klaussmann</strong></h3><p></p>`,
+    `
+    <p>Elle avait cependant fini par mettre en pause ses activités pour s'occuper de son fils.</p>
+    <h3><strong>Chapitre 2 : Enfance</strong></h3>
+    <p>Pendant son enfance, Trystan fut éduqué convenablement, il appris rapidement à lire, à chasser et à trouver des plantes. En terme de physique, il n'avais qu'un seul talent, son agilité, ce qu'il compensait avec sa rapidité et sa force médiocre. Ses parents avaient décidé de ne pas l'instruire sur l'histoire du Titan Assaillant, souhaitant qu'il conserve son innocence jusqu'au moment ou il devrait en hériter. En parlant d'innocence, Trystan, avait une façon de pensé qui différais un peu de sa famille, Tout ce qu'il rêvait, c'était de pouvoir vivre comme il le voulait, sans avoir de contraintes ou autres. C'est un enfant qui rêve énormément, il souhaite devenir comme son Grand Père, Marco Klaussmann qui pour lui était un peu comme un symbole de cette liberté, il révait déjà de rentrer dans le bataillon d'exploration et de partir en expédition comme lui.</p>
+    <button onclick="PageSuivante(1)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>Chapitre 3 : Le grand départ</strong></h3>
+    <div class="p-special2"><p>Trystan venait d'atteindre la barre des 12 ans soit l'âge requis pour rejoindre l'armée et il décida donc de prévenir ses parents de son choix final il ne voulait pas détourner de son objectif, le bataillon d'exploration était sa priorité. Le soir, pendant le diner, il se leva et annonça la nouvelle à ses parents. Leurs réactions mirent du temps à arriver, loin d'être étonnant, ils savaient depuis longtemps qu'il souhaitait rejoindre le bataillon, et force d'avoir tenter de le raisonner, en vain, ils avaient fini par accepter, cependant, sa mère lui demanda une condition afin qu'elle accepte sa demande, si Trystan arrivait dans le top 10 de sa promotion et donc qu'il avait la possibilité de rejoindre les brigades Spéciales, il devrait rejoindre ce régiment. Au bout de plusieurs minutes de réflexion, Trystan accepta la conditions et il partit directement préparer ses affaires.</p></div>    
+        <button onclick="PageSuivante(2)">Page Précedente</button>
+    </div>
+    `,
+    `
+    `,
+];
+content_page2 = [
+    `<h3><strong>Chapitre 1 : Trystan Vurst</strong></h3><br><div class="p-special0"><p>Trystan était né le 26 septembre de l'an 830 dans le village de Dauper, un petit village de chasseur dans l'enceinte du Mur Rose. En parlant de chasseur, il est issue lui aussi d'une famille de chasseur, mise à part son Grand père, Marco Klaussmann qui avais servi dans l'armée au sein du bataillon d'Exploration jusqu'au grade d'Adjudant et qui avais fait partie des premiers soldats à porter un équipement tridimensionnel et à éliminer un Titan. Son père, Thomas Vurst, se rendait régulièrement à Trost afin de vendre les produits de la chasse tel que les peaux de bêtes, ou encore la viande ou tout autres produit pouvant être vendu. Sa mère était médecin, elle avait comme réputation de toujours soigner parfaitement bien avec une excellente connaissance des plantes médecinale.</p></div><br><div class="flex-box"><button onclick="PageSuivante(2)">Page Suivante</button></div>`,
+    `
+    <p class="p-special1">Plus le temps passait, plus Trystan grandissait à vue d'oeil, ses cheveux abordés une teinte bien plus noire, et il était possible de remarquer nettement que la couleur de ses yeux était verte avec un petite lueur de bleu. Trystan n'avais pas énormément d'amis, mise à part son meilleur ami actuellement, Heinz Schnitzer, Heinz venait du village de Ragako, ils se connaissaient car Marco Klaussmann et Werner Schnitzer, le grand père de Heinz étaient amis et camarade dans le bataillon d'exploration. Heinz venait donc de temps en temps au village de Dauper pour voir Trystan et Trystan de temps en temps se rendait au village de Ragako, ce qui lui permettait de voyager un minimum et de ne plus se sentir enfermer dans sa forêt comme il en avais l'habitude. Les deux amis n'avais qu'un seul point en commun, ils voulaient touts les deux rejoindre le Bataillon d'exploration, le reste, ils diffèrent complétement puisqu'Heinz avait un physique bien plus solide que celui de Trystan.</p>
+    <div class="flex-box">
+        <button onclick="PageSuivante(3)">Page Suivante</button>
+    </div>
+    `,
+    `
+    <p> Pendant qu'il était entrain de les préparer, son père toqua à sa porte et lui demanda de le suivre, la suite, il ne s'en souviendra pas. <a href="/spinoff.html">(Voir le spin-off)</a> </p>
+    
+    <p>Trystan se reveilla doucement, du au fait qu'il avait l'impression de bouger dans tout les sens, une fois les yeux complétement ouvert, il remarqua qu'il était dans une charette, hors, il ne se rappel pas avoir monté dans une charette, en fait, il ne se rappel plus de rien depuis hier soir, le seul truc dont il se rappelait, c'était d'avoir suivi don père après qu'il eut appelé. Trystan mis la tête en dehors de la charrette et remarqua au loin le district de Shiganshina. L'aventure allait enfin pouvoir commencé...</p></br>
+    <h3><strong><center>Fin</center></strong></h3>
+    `,
+];
+
+
+function PageSuivante(nb) {
+    document.getElementById("page1").innerHTML = content_page1[nb - 1];
+    document.getElementById("page2").innerHTML = content_page2[nb - 1];
+};
+
+setTimeout(function() {
+    PageSuivante(1);
+}, 10);
